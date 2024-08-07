bfe6d092f1e404ef175c2eef8616a672bfaff17b
“mtstechnologies” <michael.trindade@hotmail.com>
2024-08-07 00:27:17 -0400

    Project created

---
+++ src/main/java/com/mts/SantanderDevWeekApiApplication.java
@@ -0,0 +1,13 @@
+package com.mts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeekApiApplication.class, args);
+	}
+
+}
